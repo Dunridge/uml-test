535bdc0b6d30993564d4fbb06f3c0857230f14eb
Dunridge <Polinka7max@gmail.com>
2021-03-16 09:35:07 +0200

    first commit

---
+++ src/main/java/com/max/production/Main.java
@@ -0,0 +1,15 @@
+package com.max.production;
+
+/*
+* Notes: (View diagram on a package﻿)
+1) In the Project tool window, right-click a package for which you want to create a diagram and select
+* Diagrams | Show Diagram Ctrl+Alt+Shift+U ).
+2) In the list that opens, select Java Class Diagram. IntelliJ IDEA generates a UML diagram for classes and their dependencies.
+*
+* */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
